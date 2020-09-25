bb1ed196b0d5173a0be3095b0b1a8c1deca7b77a
Kendra Kaiser <kendra.kaiser@gmail.com>
2020-09-25 15:20:04 -0600

    Added agrimet data to full temperature data, some things are hard coded in that could be improved

--- code/temperature_model.R
+++ code/temperature_model.R
@@ -21,7 +21,7 @@ agrimet = read.csv(file.path(cd,'agri_metT.csv'))
 first.yr<-1988
 last.yr<-2020
 nyrs<-last.yr-first.yr+1
-site.key = as.character(unique(snotel$site_name))
+site.key <- c(as.character(unique(snotel$site_name)), as.character(unique(agrimet$site_name)))
 
 #create a dataframe to store avg. Apr/Jun Temp for each site for period of record
 tdata<-data.frame(array(NA,c(length(site.key)*nyrs,3)))
@@ -30,11 +30,21 @@ tdata$year<-rep(first.yr:last.yr,length(site.key))
 tdata$site<-rep(site.key,each=nyrs)
 
 #calculate average april/jun temperature for every year
-for(i in 1:length(site.key)){
+for(i in 1:10){ #hard coded this in after adding agrimet sites to site.key list
   for (y in first.yr:last.yr){
     sub<- snotel[snotel$site_name == site.key[i] & snotel$wy==y, ] #subset to indv. site and year
     #average april - june temps
     mean.temp <- mean(sub[sub$mo == 4 | sub$mo ==5 | sub$mo ==6 , "temperature_mean"], na.rm=T)
+    #save to tdata table
+    tdata$Apr.Jun.tempF[tdata$year == y & tdata$site == site.key[i]] <- mean.temp
+  }
+}
+
+for(i in 11:12){
+  for (y in first.yr:last.yr){
+    sub<- na.omit(agrimet[agrimet$site_name == site.key[i] & agrimet$y==y, ]) #subset to indv. site and year
+    #average april - june temps
+    mean.temp <- mean(sub[sub$month == 4 | sub$month ==5 | sub$month ==6 , "t"], na.rm=T)
     #save to tdata table
     tdata$Apr.Jun.tempF[tdata$year == y & tdata$site == site.key[i]] <- mean.temp
   }
