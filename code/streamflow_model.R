23d6394f6990b660ed974ca8555625914d6717b5
Kendra Kaiser <kendra.kaiser@gmail.com>
2020-10-27 17:34:47 -0600

    Added summary table

--- code/streamflow_model.R
+++ code/streamflow_model.R
@@ -33,6 +33,34 @@ wy<-data.frame(wy,1:365)
 colnames(wy)<-c("Date","day")
 
 # ------------------------------------------------------------------------------ # 
+# Setup output arrays
+# ------------------------------------------------------------------------------ # 
+
+# volumes
+output.vol<-array(NA,c(length(stream.id),8))
+rownames(output.vol)<-stream.id
+colnames(output.vol)<-c("cfs % of mean","swe % of mean", "ask RVK fit", "ask RVK fit", "ask RVK fit", "ask RVK fit", "ask RVK fit", "Prev Year % of mean Volume")
+pred.params.vol<-array(NA,c(5,2))
+rownames(pred.params.vol)<-c("bwb.vol","bws.vol","cc.vol","bwr.vol","sc.vol")
+colnames(pred.params.vol)<-c("log.vol","sigma")
+
+# center of mass
+output.cm<-data.frame(array(NA,c(5,5)))
+rownames(output.cm)<-stream.id
+colnames(output.cm)<-c("temp-mean","% of mean","cm","cm-mean","cm.date")
+pred.params.cm<-array(NA,c(5,2))
+rownames(pred.params.cm)<-c("bwb.cm","bws.cm","cc.cm","bwr.cm","sc.cm")
+colnames(pred.params.cm)<-c("cm","sigma")
+
+swe.new<-data.frame(t(c(rep(NA,10))))
+names(swe.new)<-c("cg", "g", "gs", "hc", "lwd", "ds", "ccd", "sr", "ga", "sp")
+
+# summary stats
+mod_sum<-data.frame(array(NA,c(4,2)))
+colnames(mod_sum)<-c("Vol Adj-R2", "CM ADj-R2")
+rownames(mod_sum)<-c("bwb","bws","cc","sc")
+
+# ------------------------------------------------------------------------------ # 
 #
 # April-Sept Volume Predictions
 #
@@ -79,6 +107,7 @@ modOut<- function(mod, pred.dat, wq, vol, meanSWE, lastQ){
 hist <- var[var$year < pred.yr,] %>% select(cc.vol, cc.wq, ccd.swe, sr.swe) 
 # Camas Creek linear model
 cc_mod<-lm(log(cc.vol)~log(cc.wq)+sr.swe+ccd.swe, data=hist) 
+mod_sum[3,1]<-summary(cc_mod)$adj.r.squared
 
 #April 1 data to use for prediction 
 pred.dat<-data.frame(array(NA,c(1,3)))
@@ -98,6 +127,7 @@ pred.params.vol[3,] <- mod_out[[2]]
 hist <- var[var$year < pred.yr,] %>% select(bwb.vol, bwb.wq, g.swe, gs.swe, hc.swe) 
 # Big Wood at Hailey linear model
 bwb_mod<-lm(log(bwb.vol)~log(bwb.wq)+log(g.swe)+ log(gs.swe)+ log(hc.swe), data=hist) 
+mod_sum[1,1]<-summary(bwb_mod)$adj.r.squared
 
 #April 1 data to use for prediction 
 pred.dat<-data.frame(array(NA,c(1,6)))
@@ -120,6 +150,7 @@ pred.params.vol[1,] <- mod_out[[2]]
 hist <- var[var$year < pred.yr & var$year > 1996,] %>% select(bws.vol, bws.wq, cg.swe, g.swe, gs.swe, hc.swe, lwd.swe) 
 # Big Wood at Stanton linear model
 bws_mod<-lm(log(bws.vol)~bws.wq+g.swe+ log(gs.swe)+ log(hc.swe), data=hist) 
+mod_sum[2,1]<-summary(bws_mod)$adj.r.squared
 
 #April 1 data to use for prediction 
 pred.dat<-data.frame(array(NA,c(1,6)))
@@ -143,6 +174,7 @@ pred.params.vol[2,] <- mod_out[[2]]
 hist <- var[var$year < pred.yr,] %>% select(sc.vol, sc.wq, ga.swe, sp.swe) 
 # Silver Creek linear model -- note no log tranformations here, difference in travel times?
 sc_mod<-lm(sc.vol~sc.wq+ga.swe+sp.swe, data=hist) 
+mod_sum[4,1]<-summary(sc_mod)$adj.r.squared
 
 #April 1 data to use for prediction 
 pred.dat<-data.frame(array(NA,c(1,3)))
@@ -166,24 +198,25 @@ pred.params.vol[5,] <- mod_out[[2]]
 hist <- var[var$year < pred.yr,] %>% select(cc.cm, ccd.swe, t.sr) 
 # Camas Creek linear model
 cc_mod.cm<-lm(log(cc.cm)~log(ccd.swe) + t.sr, data=hist) 
-summary(cc_mod.cm) #R2 is a sweet 0.40
+mod_sum[3,2]<-summary(cc_mod.cm)$adj.r.squared 
 
 
 #big wood at hailey
 hist <- var[var$year < pred.yr,] %>% select(bwb.cm, cg.swe, g.swe, gs.swe, hc.swe, lwd.swe, t.cg, t.g, t.gs, t.hc, t.lw) 
 #linear model
 bwb_mod.cm <-lm(log(bwb.cm) ~ g.swe+t.cg+ t.g+t.gs+t.hc+t.lw +log(cg.swe)+log(hc.swe), data=hist)
-summary(bwb_mod.cm) #r2 at 0.947
+mod_sum[1,2]<-summary(bwb_mod.cm)$adj.r.squared #r2 at 0.947
 
 
 #big wood at stanton
 hist <- var[var$year < pred.yr,] %>% select(bws.cm, cg.swe, g.swe, gs.swe, hc.swe, lwd.swe, t.cg, t.g, t.gs, t.hc, t.lw) 
 #linear model
 bws_mod.cm <-lm(log(bws.cm) ~ g.swe + t.cg+ t.g +t.lw +log(cg.swe)+log(gs.swe)+log(hc.swe), data=hist)
-summary(bws_mod.cm) #r2 at 0.914
+mod_sum[2,2]<-summary(bws_mod.cm)$adj.r.squared #r2 at 0.914
+
 
 # Subset Silver Creek Winter flows, Snotel from Garfield Ranger Station and Swede Peak
 hist <- var[var$year < pred.yr,] %>% select(sc.cm, sc.wq, ga.swe, sp.swe, t.sp) 
 # Silver Creek linear model -- note no log tranformations here, difference in travel times?
 sc_mod.cm<-lm(sc.cm~log(sc.wq)+ga.swe+log(sp.swe), data=hist) 
-summary(sc_mod.cm) #r2 0.134 this does terribly ... 
+mod_sum[4,2]<-summary(sc_mod.cm)$adj.r.squared #r2 0.134 this does terribly ... 
