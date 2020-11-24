7a6cb655e896bbfe4fb18e2d0845961c512f4cef
Kendra Kaiser <kendrakaiser@boisestate.edu>
2020-11-24 12:01:46 -0700

    All packages needed to run data download and model

---
+++ code/packages.R
@@ -0,0 +1,24 @@
+## packages.R
+# All packages needed to run WRWC scripts
+
+# load packages
+
+library(MASS)
+library(plotrix)
+library(mvtnorm)
+library(tidyverse)
+
+library(dataRetrieval)
+library(tidyverse)
+library(snotelr)
+library(XML)
+library(httr)
+library(dplyr)
+library(devtools)
+library(plyr)
+library(readr)
+library(lubridate)
+library(lfstat)
+
+devtools::install_github(repo = "rhlee12/RNRCS", subdir = "/RNRCS/", force =TRUE)
+library(RNRCS)
