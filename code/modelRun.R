f4b3f78245028317fca62a63b95743fbb6ce5113
Kendra Kaiser <kendrakaiser@boisestate.edu>
2020-11-24 12:01:22 -0700

    Start of wrapper script to run all model components

---
+++ code/modelRun.R
@@ -0,0 +1,7 @@
+# ModelRun.R
+# Script to wrap all data downloading and model runs into one script
+
+
+source(file.path("code", "data_scraping.R"))
+source(file.path("code", "temperature_model.R"))
+source(file.path("code", "streamflow_model.R"))
