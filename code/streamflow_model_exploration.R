1843b0db2088528b46b99edbc23073f607a508bb
Kendra Kaiser <kendrakaiser@boisestate.edu>
2020-12-17 12:49:12 -0700

    updated data period for diversion data

--- code/streamflow_model_exploration.R
+++ code/streamflow_model_exploration.R
@@ -94,7 +94,7 @@ plot(rs$bic, rs$adjr2, xlab="BIC", ylab="adj R2")
 
 # -------------------------------------------------------------
 # Diversions above Hailey
-hist <- var[var$year >= 2000 & var$year < pred.yr,] %>% select(bwb.wq, cg.swe, g.swe, gs.swe, hc.swe, lwd.swe, t.cg, t.g, t.gs, t.hc, t.lw, year) 
+hist <- var[var$year >= 1997 & var$year < pred.yr,] %>% select(bwb.wq, cg.swe, g.swe, gs.swe, hc.swe, lwd.swe, t.cg, t.g, t.gs, t.hc, t.lw, year) 
 hist$log.wq <- log(hist$bwb.wq)
 hist$log.cg<- log(hist$cg.swe)
 hist$log.g <- log(hist$g.swe)
@@ -102,13 +102,13 @@ hist$log.gs<- log(hist$gs.swe)
 hist$log.hc <- log(hist$hc.swe)
 hist$log.lwd <- log(hist$lwd.swe)
 #use regsubsets to plot the results
-regsubsets.out<-regsubsets(var$abv.h[var$year >= 2000 & var$year < pred.yr]~., data=hist, nbest=3, nvmax=8)
-# g.swe, hc.swe, t.cg, t.lw post 2000, no trend with time
+regsubsets.out<-regsubsets(var$abv.h[var$year >= 1997 & var$year < pred.yr]~., data=hist, nbest=3, nvmax=8)
+# cg.swe, g.swe, hc.swe, t.cg, t.g, t.gs, t.lw, log.lwd, r2=.65 post 1997, no trend with time
 
 # Diversions above Stanton Crossing
 plot(var$year, var$abv.s)
-hist <- var[var$year >= 2000 & var$year < pred.yr,] %>% select(bwb.wq, bws.wq,, bwb.vol.nat, bws.vol.nat, cg.swe, g.swe, gs.swe, hc.swe, lwd.swe, t.cg, t.g, t.gs, t.hc, t.lw, year) 
-regsubsets.out<-regsubsets(var$abv.s[var$year >= 2000 & var$year < pred.yr]~., data=hist, nbest=3, nvmax=8)
+hist <- var[var$year >= 1997 & var$year < pred.yr,] %>% select(bwb.wq, bws.wq,, bwb.vol.nat, bws.vol.nat, cg.swe, g.swe, gs.swe, hc.swe, lwd.swe, t.cg, t.g, t.gs, t.hc, t.lw, year) 
+regsubsets.out<-regsubsets(var$abv.s[var$year >= 1997 & var$year < pred.yr]~., data=hist, nbest=3, nvmax=8)
 # these all do really poorly, and no trends in the data at all, so pull randomly
 
 # Losses Between Hailey and Stanton Crossing
@@ -119,6 +119,18 @@ hist <- var[var$year >= 2000 & var$year < pred.yr,] %>% select(bwb.wq, cg.swe, g
 regsubsets.out<-regsubsets(var$bws.loss[var$year >= 2000 & var$year < pred.yr]~., data=hist, nbest=2, nvmax=8)
 # t.cg, t.gs, lowest BIC, R2 == 0.55
 # g.swe, hc.swe, t.g, t.cg, t.gs highest R2 and third lowest BIC
+
+# Total Diversions
+hist <- var[var$year >= 1997 & var$year < 2020,] %>% select(bwb.wq, cg.swe, g.swe, gs.swe, hc.swe, lwd.swe, t.cg, t.g, t.gs, t.hc, t.lw, year) 
+hist$log.wq <- log(hist$bwb.wq)
+hist$log.cg<- log(hist$cg.swe)
+hist$log.g <- log(hist$g.swe)
+hist$log.gs<- log(hist$gs.swe)
+hist$log.hc <- log(hist$hc.swe)
+hist$log.lwd <- log(hist$lwd.swe)
+hist<-hist %>% select(-cg.swe, -hc.swe, -bwb.wq)
+regsubsets.out<-regsubsets(var$div[var$year >= 1997 & var$year < 2020]~., data=hist, nbest=3, nvmax=8)
+
 
 # ------------------------------------------------------------------------------ # 
 # Evaluate alternative model combinations for Center of Mass Predictions
