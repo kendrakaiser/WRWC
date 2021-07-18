87b27c0f63f8e777f7cb755ebf1de6f0f7f3f29d
Kendra Kaiser <kendrakaiser@boisestate.edu>
2021-07-18 16:32:17 -0600

    Edited comments

--- code/temperature_models.R
+++ code/temperature_models.R
@@ -21,9 +21,9 @@ nyrs<-last.yr-first.yr+1
 site.key <- c(as.character(unique(snotel$site_name)), as.character(unique(agrimet$site_name)))
 elev<-c(1923,1740,1750,2408,2566,2277,1999,2323,2408,2265,2676,2329,1536,1494)
 #create a dataframe to store avg. Apr/Jun Temp for each site for period of record
-tdata<-data.frame(array(NA,c(length(site.key)*nyrs,5)))
+tdata<-data.frame(array(NA,c(length(site.key)*nyrs,6)))
 #summer temp july-sept, winter temp NDJFM
-colnames(tdata)<-c("year","site","spring.tempF", "sum.tempF", "wint.tempF")
+colnames(tdata)<-c("year","site","spring.tempF", "sum.tempF", "wint.tempF", "nj.tempF")
 tdata$year<-rep(first.yr:last.yr,length(site.key))
 tdata$site<-rep(site.key, each=nyrs)
 tdata$elev<-rep(elev, each=nyrs)
@@ -36,15 +36,18 @@ for(i in 1:12){ #hard coded this in after adding agrimet sites to site.key list
     mean.temp <- mean(sub[sub$mo == 4 | sub$mo ==5 | sub$mo ==6 , "temperature_mean"], na.rm=T)
     #average summer temps July Aug Sept
     sum.mean.temp <- mean(sub[sub$mo == 7 | sub$mo ==8 | sub$mo ==9 , "temperature_mean"], na.rm=T)
-    #average april - june temps
+    #average winter temps
     wint.mean.temp <- mean(sub[sub$mo == 11 | sub$mo ==12 | sub$mo ==1 | sub$mo ==2 | sub$mo ==3 , "temperature_mean"], na.rm=T)
+    #average nov-jan temps
+    nj.mean.temp <- mean(sub[sub$mo == 11 | sub$mo ==12 | sub$mo ==1, "temperature_mean"], na.rm=T)
     #save to tdata table
     tdata$spring.tempF[tdata$year == y & tdata$site == site.key[i]] <- mean.temp
     tdata$sum.tempF[tdata$year == y & tdata$site == site.key[i]] <- sum.mean.temp
     tdata$wint.tempF[tdata$year == y & tdata$site == site.key[i]] <- wint.mean.temp
+    tdata$nj.tempF[tdata$year == y & tdata$site == site.key[i]] <- nj.mean.temp
   }
 }
-#calculate Agrimet average april/jun temperature for every year
+#calculate Agrimet average seasonal temperatures for every year
 for(i in 13:14){# these values could be ∆ to not be hard coded
   for (y in first.yr:last.yr){
     sub<- na.omit(agrimet[agrimet$site_name == site.key[i] & agrimet$y==y, ]) #subset to indv. site and year
@@ -52,12 +55,16 @@ for(i in 13:14){# these values could be ∆ to not be hard coded
     mean.temp <- mean(sub[sub$month == 4 | sub$month ==5 | sub$month ==6 , "t"], na.rm=T)
     #average summer temps July Aug Sept
     sum.mean.temp <- mean(sub[sub$mo == 7 | sub$mo ==8 | sub$mo ==9 , "t"], na.rm=T)
-    #average april - june temps
+    #average winter temps
     wint.mean.temp <- mean(sub[sub$mo == 11 | sub$mo ==12 | sub$mo ==1 | sub$mo ==2 | sub$mo ==3 , "t"], na.rm=T)
+    #average nov-jan temps
+    nj.mean.temp <- mean(sub[sub$mo == 11 | sub$mo ==12 | sub$mo ==1, "t"], na.rm=T)
+    
     #save to tdata table
     tdata$spring.tempF[tdata$year == y & tdata$site == site.key[i]] <- mean.temp
     tdata$sum.tempF[tdata$year == y & tdata$site == site.key[i]] <- sum.mean.temp
     tdata$wint.tempF[tdata$year == y & tdata$site == site.key[i]] <- wint.mean.temp
+    tdata$nj.tempF[tdata$year == y & tdata$site == site.key[i]] <- nj.mean.temp
   }
 }
 
@@ -65,14 +72,17 @@ for(i in 13:14){# these values could be ∆ to not be hard coded
 spring.tdata <-pivot_wider(tdata[,1:3], names_from = site, values_from = spring.tempF)
 sum.tdata<-pivot_wider(tdata[,c(1,2,4)], names_from = site, values_from = sum.tempF)
 wint.tdata<-pivot_wider(tdata[,c(1,2,5)], names_from = site, values_from = wint.tempF)
+nj.tdata<-pivot_wider(tdata[,c(1,2,6)], names_from = site, values_from = nj.tempF)
 colnames(spring.tdata)<-c("year", "t.cg","t.ccd", "t.sr", "t.bc","t.ds","t.g","t.ga", "t.hc", "t.lw", "t.sm", "t.gs", "t.sp","t.p", "t.f")
 colnames(sum.tdata)<-c("year", "t.cg","t.ccd", "t.sr", "t.bc","t.ds","t.g","t.ga", "t.hc", "t.lw", "t.sm", "t.gs", "t.sp","t.p", "t.f")
 colnames(wint.tdata)<-c("year", "t.cg","t.ccd", "t.sr", "t.bc","t.ds","t.g","t.ga", "t.hc", "t.lw", "t.sm", "t.gs", "t.sp","t.p", "t.f")
+colnames(nj.tdata)<-c("year", "t.cg","t.ccd", "t.sr", "t.bc","t.ds","t.g","t.ga", "t.hc", "t.lw", "t.sm", "t.gs", "t.sp","t.p", "t.f")
 
 
 write.csv(spring.tdata, file.path(data_out, 'sprTemps.csv'))
 write.csv(sum.tdata, file.path(data_out, 'sumTemps.csv'))
 write.csv(wint.tdata, file.path(data_out, 'wintTemps.csv'))
+write.csv(nj.tdata, file.path(data_out, 'njTemps.csv'))
 
 snotel_abrv <- c("cg", "g", "gs", "hc", "lwd", "ds", "ccd", "sr", "ga", "sp")
 #plot all data
@@ -108,7 +118,7 @@ nboot<-5000
 # All sites winter --------------------------------------------------------------------------
 tdata.sno<- tdata[tdata$site != "fairfield" & tdata$site != "picabo",]
 #not so sure about this .. come back to decide how it would be used (e.g. summer temp goes into irrigation est)
-trend.reml2<-lme(fixed=sum.tempF ~ year, random=~1+year|site, correlation = corAR1(), data=tdata.sno, method="REML",na.action=na.omit)
+trend.reml<-lme(fixed=wint.tempF ~ year +elev, random=~1+year|site, correlation = corAR1(), data=tdata.sno, method="REML",na.action=na.omit)
 summary(trend.reml)
 
 # predict this years temperature
@@ -120,10 +130,14 @@ fits<-fitted(trend.reml,0:1)[(1:nyrs),1]
 mu<-trend.reml$coef$fixed
 sig<-trend.reml$var
 rand.coefs<-mvrnorm(nboots,mu,sig)
-var.est<-var(rand.coefs%*%c(1,last.yr+1))
+var.est<-var(rand.coefs%*%c(1,last.yr+1, 2121))
 var.site<-var(summary(trend.reml)$coeff$random$site[,1])/length(site.key)
-se.pred<-sqrt(var.est+var.site)
+se.pred<-sqrt(var.est+var.site) #standard error
 sum.temps.all<-rnorm(nboot,mean=pred,sd=se.pred)
+
+plot(tdata.sno$year,tdata.sno$wint.tempF,xlab="Year",xlim=c(first.yr,(last.yr+1)),
+     ylab="Temperature (F)",main="Mean Winter Temperature")
+lines(first.yr:(last.yr+1),c(fits,pred),lwd=3)
 
 # site combinations needed for multivariate models
 
