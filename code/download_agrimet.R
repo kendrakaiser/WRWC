84079cd8fdb448a9dffc8ae3ca1409d6a7721ca2
Kendra Kaiser <kendrakaiser@geo-kk-imac.boisestate.edu>
2022-08-10 12:38:43 -0600

    changed water year function

--- code/download_agrimet.R
+++ code/download_agrimet.R
@@ -79,10 +79,10 @@ colnames(piciT)<- c("date_time","t", "site_name", 'month', 'y')
 
 # Merge & save AgriMet Data ---------------------------------
 agri_metT <- rbind(piciT, fafiT)
-agri_metT$wy[!is.na(agri_metT$date_time)]<- as.numeric(as.character(water_year(agri_metT$date_time[!is.na(agri_metT$date_time)], origin='usgs')))
+agri_metT$wy[!is.na(agri_metT$date_time)]<- as.numeric(as.character(waterYear(agri_metT$date_time[!is.na(agri_metT$date_time)], numeric=TRUE)))
 
 agri_met<- full_join(pici, fafi, by='date_time')
-agri_met$wy[!is.na(agri_met$date_time)]<- as.numeric(as.character(water_year(agri_met$date_time[!is.na(agri_met$date_time)], origin='usgs')))
+agri_met$wy[!is.na(agri_met$date_time)]<- as.numeric(as.character(waterYear(agri_met$date_time[!is.na(agri_met$date_time)], numeric=TRUE)))
 
 # saving to local directory
 write.csv(agri_metT, file.path(data_dir,'agri_metT.csv'), row.names = FALSE)
