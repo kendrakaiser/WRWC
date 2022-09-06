4b2de3620317b5519e16a6387349f2ddcb41bb9c
kendrakaiser <kendra.kaiser@gmail.com>
2022-09-06 13:19:48 -0600

    fixed bws volume from prior to gage starting

--- code/streamflow_models.R
+++ code/streamflow_models.R
@@ -18,7 +18,8 @@ usgs_sites = read.csv(file.path(data_dir,'usgs_sites.csv'))
 stream.id<-unique(as.character(usgs_sites$abv))
 
 swe_q = read.csv(file.path(data_dir,input_data))
-swe_q[swe_q == 0] <- 0.01 # change zeros to a value so lm works FIX THIS it changes BWS vol to 0 prior to the gage being on 
+swe_q$bws.vol[swe_q$year <=1996]<- NA
+swe_q[swe_q == 0] <- 0.01 # change zeros to a value so lm works 
 swe_q<-swe_q[!(names(swe_q) %in% c("bwb.cm.nat","bws.cm.nat","bwb.vol.nat","bws.vol.nat","bws.loss","sc.vol.nat"))]
 
 spring.temps = read.csv(file.path(data_dir, 'sprTemps.csv'))
