feb296c953137be6809c7e5717c5a3156a1ea232
Kendra Kaiser <kendrakaiser@boisestate.edu>
2023-02-22 09:27:13 -0700

    changed CM year smaple to data frame

--- code/streamflow_predictions.R
+++ code/streamflow_predictions.R
@@ -355,7 +355,7 @@ png(file.path(fig_dir_mo,"ex.vols.png"), height = 30*nrow(ex.vols), width = 130*
 grid.table(ex.vols, theme = my_table_theme, rows = NULL)
 dev.off()
 
-saveRDS(ex.vols, file.path(fig_dir_mo,"ex.vols.rds"))
+saveRDS(round(ex.vols,0), file.path(fig_dir_mo,"ex.vols.rds"))
 ex.vols3 <- ex.vols %>%pivot_longer(!Exceedance, names_to="site", values_to="value")
 ex.vols3$t<- "Predicted"
 # ------------------------------------------------------------------------------
@@ -478,7 +478,7 @@ cm_sum<- cm_sum*100
 png(file.path(fig_dir_mo,"CM_summary.png"), height = 50*nrow(cm_sum), width = 200*ncol(cm_sum))
 grid.table(cm_sum)
 dev.off()
-
+CMyear.sample<-as.data.frame(CMyear.sample)
 #save the probabilities for use in the simulation
 write.csv(CMyear.sample, file.path(model_out, paste0("CMyear.sample-", end_date,".csv")),row.names=F)
 
