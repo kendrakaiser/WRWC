dc8c2716686ddc6fed0e1369dae0b3984302bbc6
Kendra Kaiser <kendrakaiser@boisestate.edu>
2023-02-27 08:58:05 -0700

    Added note

--- code/streamflow_models.R
+++ code/streamflow_models.R
@@ -11,7 +11,7 @@
 defaultW <- getOption("warn") 
 options(warn = -1) 
 
-# Import Data ------------------------------------------------------------------ # 
+# Import & Compile Data -------------------------------------------------------# 
 # Streamflow, Current SWE, historic and Modeled Temperature Data
 #q = read.csv(file.path(cd,'streamflow_data.csv'))
 usgs_sites = read.csv(file.path(data_dir,'usgs_sites.csv'))
