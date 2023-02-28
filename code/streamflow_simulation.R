8153161eeb648984f2a507a9cd6894e5ac77d9e4
Kendra Kaiser <kendrakaiser@boisestate.edu>
2023-02-28 15:50:42 -0700

    added curtailment figure

--- code/streamflow_simulation.R
+++ code/streamflow_simulation.R
@@ -206,33 +206,67 @@ write.csv(sc.flow.s, file.path(model_out, paste0("SC.sim-", end_date,".csv")), r
 wr.cutoffs<- data.frame("date" = array(NA,c(183)))
 wr.cutoffs$date<- dates
 wr.cutoffs$doy<- yday(dates)
-wr.cutoffs$bws.meanQ<-pi[,"bwb.mean"]
 
+wr.cutoffs$bwb.meanQ<-pi[,"bwb.mean"]
+wr.cutoffs$bwb.medQ<-pi[,"bwb.med"]
+wr.cutoffs$bwb.hiQ<-pi[,"bwb.hi"]
+wr.cutoffs$bwb.lowQ<-pi[,"bwb.low"]
 
 # find the date of the most senior wr that is above the "current" flow
 cutoff<- function(flow, wr_array){
   cut.wr<- wr_array$priority.date[min(which(wr_array$cuml.cfs > flow))]
   return(cut.wr)
   }
+
+#clean this up to shorten this 
+wr.cutoffs$cut.wr.hi<-Reduce(c, lapply(wr.cutoffs$bwb.hiQ, cutoff, wr.sub))
+#wr.cutoffs$cut.wr.hi[1:which(wr.cutoffs$bwb.hiQ == max(wr.cutoffs$bwb.hiQ))]<-NA #ignore cutoffs before peak
+
+wr.cutoffs$cut.wr.med<-Reduce(c, lapply(wr.cutoffs$bwb.medQ, cutoff, wr.sub))
+#wr.cutoffs$cut.wr.med[1:which(wr.cutoffs$bwb.medQ == max(wr.cutoffs$bwb.medQ))]<-NA #ignore cutoffs before peak
+
+wr.cutoffs$cut.wr.low<-Reduce(c, lapply(wr.cutoffs$bwb.lowQ, cutoff, wr.sub))
+#wr.cutoffs$cut.wr.low[1:which(wr.cutoffs$bwb.lowQ == max(wr.cutoffs$bwb.lowQ))]<-NA #ignore cutoffs before peak
+
+wr.cutoffs$ymo.hi<- zoo::as.yearmon(wr.cutoffs$cut.wr.hi)
+wr.cutoffs$ymo.med<- zoo::as.yearmon(wr.cutoffs$cut.wr.med)
+wr.cutoffs$ymo.low<- zoo::as.yearmon(wr.cutoffs$cut.wr.low)
+
+ymo.list<-unique(c(wr.cutoffs$ymo.hi,wr.cutoffs$ymo.med,wr.cutoffs$ymo.low))
+ymo.list<- ymo.list[order(ymo.list)]
+
+
+
+wr.ts<- function(wr.co, flow, sitename="name"){
+  wr.co[sitename]<- flow
+  wr.co$cut.wr<-Reduce(c, lapply(wr.co[sitename], cutoff, wr.sub))
+  wr.co$cut.wr[1:which(wr.co[sitename] == max(wr.co[sitename]))]<-NA #ignore cutoffs before peak
+  wr.co$ymo<- zoo::as.yearmon(wr.co$cut.wr)
+  wr.co- wr.co[complete.cases(wr.co),]
   
-wr.cutoffs$cut.wr<-Reduce(c, lapply(wr.cutoffs$bws.meanQ, cutoff, wr.sub))
-wr.cutoffs$cut.wr[1:which(wr.cutoffs$bws.meanQ == max(wr.cutoffs$bws.meanQ))]<-NA #ignore cutoffs before peak
-wr.cutoffs$ymo<- zoo::as.yearmon(wr.cutoffs$cut.wr)
-wr.cutoffs<- wr.cutoffs[complete.cases(wr.cutoffs),]
+  return(wr.co)
+}
+#wr.cutoffs<- wr.ts(wr.cutoffs, pi[,"bwb.med"], sitename = "bws.medQ")
 
 
-ggplot(wr.cutoffs, aes(x=date, y=bws.meanQ, color= as.factor(ymo))) + 
-  geom_point()+
+ggplot(data=wr.cutoffs) + 
+  geom_point(aes(x=date, y=bwb.hiQ, color= as.factor(ymo.hi)), show.legend = FALSE)+
+  scale_color_viridis(discrete = TRUE, option = "turbo", direction=-1, limits = as.factor(ymo.list))+ 
+  new_scale_colour() +
+  geom_point(aes(x=date, y=bwb.medQ, color= as.factor(ymo.med)), show.legend = FALSE)+
+  scale_color_viridis(discrete = TRUE, option = "turbo", direction=-1, limits = as.factor(ymo.list))+  
+  new_scale_colour() +
+  geom_point(aes(x=date, y=bwb.lowQ, color= as.factor(ymo.low)))+
+  scale_color_viridis(discrete = TRUE, option = "turbo", direction=-1, limits = as.factor(ymo.list))+  
   theme_bw()+
   scale_y_continuous(n.breaks=8)+
-  scale_x_date(date_breaks = "weeks" , date_labels = "%m/%d")+
-  scale_color_viridis(discrete = TRUE, option = "turbo", direction=-1)+  
-  labs(color = "Water Right")+
+  scale_x_date(date_breaks = "weeks" , date_labels = "%m/%d", limits=c(wr.cutoffs$date[1], wr.cutoffs$date[183]))+
+  labs(color = "Surface Water Rights")+
   ylab("Average Simulated Streamflow @ Hailey (cfs)")+
   xlab("Date")
 
 ggsave(filename = file.path(fig_dir_mo, "curtailment_recession.png"),
-    width = 8.5, height = 5.5, units = "in", pointsize = 12,
+    width = 14.5, height = 5.5, units = "in", pointsize = 12,
     bg = "white") 
 
 #the big problem / question here is that the old water rights shouldnt get turned off, so I'm wondering which data should be used here??
