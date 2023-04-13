0483b15e15b113fe4ff49e288960da29ff0dbbb8
Kendra Kaiser <kendrakaiser@boisestate.edu>
2023-04-13 15:54:27 -0600

    modifying function

--- code/get_snow.R
+++ code/get_snow.R
@@ -17,31 +17,35 @@ boundingbox <- dbGetQuery(conn, "SELECT ST_EXTENT(watersheds.geometry) FROM wate
 %>% st_transform(crs=st_crs(4326))
 
 #download by largest extent, then use extract w diff watersheds
-grab_ws_snow = function(ws_id, date_seq, param, metric){ #need to make work for both single date and sequence
+grab_ws_snow = function(ws_id, date, param, metric){ #need to make work for both single date and sequence
   
   #look in db to see if the metric exists
     #return metric
   
-  #if doesnt exist in database pull all data for that date / date range & calculate metrics
+  #if doesn't exist in database pull all data for that date / date range & calculate metrics
   
+  ##--------------------------------------------------------------------------#
+  ## --- Download the full SNODAS dataset
+  download.SNODAS(date)
+
+  ## --- pull spatial geometry from location ID and transform it to extent --- #
+  ws_ids = c(140,161)#c()
+  ws_geoms=st_read(conn, query=paste0("SELECT watershedgeometry FROM locationattributes WHERE locationid IN ('",
+                                      paste(ws_ids, collapse= "', '"),"');"))
   
-  ### --- pull spatial geometry from location ID and transform it to extent 
-  ws_geom=st_read(conn, query=paste0("SELECT watershedgeometry FROM locationattributes WHERE locationid=",
-                                     ws_id, ";"))
-  ws_geom_tr= st_transform(ws_geom, crs=st_crs(4326))
-  # pull extent of watershed
-  extent=matrix(st_bbox(ws_geom_tr), nrow=2)
+  # geometries of sub watersheds to use to extract metrics of interest
+  ws_geoms_tr= st_transform(ws_geoms, crs=st_crs(4326))
+  # create full extent of combined watershed area
+  ws_extent=matrix(st_bbox(ws_geom_tr), nrow=2)
+
+  ## --- extract all SNODAS values wanted 
+  out_img<-extract.SNODAS.subset(date, values_wanted=c('SWE', 'Runoff'), extent=ws_extent, write_file = FALSE) 
   
-  ### --- Download the full SNODAS dataset
-  download.SNODAS(date_seq)
-  ### --- extract all SNODAS values wanted 
-  out_img<-extract.SNODAS.subset(date_seq, values_wanted=c('SWE', 'Runoff'), extent=baseline_extent, write_file = FALSE) 
-  
-  #convert image to raster
+  #convert images to rasters -- NEED TO MODIFY TO MULT RASTERS
   out_img=rast(out_img[[1]])
   
-  #extract values from all relevant parameters and modeify into metrics
-  out_swe<-terra::extract(out_img, baseline_geomt)
+  ## --- extract values from all relevant parameters and modify into metrics ---#
+  out_swe<-terra::extract(out_img, ws_geoms_tr) ## MODIFY for stack of rasters
   tot_swe<-c(date_ts, sum(out_swe$X2023.03.16)) ## this needs to be modified
   
   #write new metrics to database
@@ -54,6 +58,7 @@ grab_ws_snow = function(ws_id, date_seq, param, metric){ #need to make work for 
 
 date_seq<-seq(as.Date("2021-10-01"), as.Date("2022-09-30"), by= "day")
 full_extent = grab_geom(161)
+date = as.Date("2023-04-11")
 
 out<- grab_ws_snow(full_extent, date_seq, param ='SWE')
 
