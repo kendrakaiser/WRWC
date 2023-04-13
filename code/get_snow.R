1d780d94ee928d6fcedb5deaec685638ac63d6d7
Kendra Kaiser <kendrakaiser@boisestate.edu>
2023-04-13 15:22:48 -0600

    made notes on how to change function

--- code/get_snow.R
+++ code/get_snow.R
@@ -12,25 +12,26 @@ library(R.utils)
 
 source("~/github/WRWC/code/init_db.R") # /Documents
 
-# create function to pull ws geometry from location ID 
-hai_id=140
-ws_id =161
+boundingbox <- dbGetQuery(conn, "SELECT ST_EXTENT(watersheds.geometry) FROM watersheds;") 
 
-# function to pull spatial data
+%>% st_transform(crs=st_crs(4326))
+
+# function to pull spatial geometry from location ID and transform it 
 grab_geom = function(ws_id){
   ws_geom=st_read(conn, query=paste0("SELECT watershedgeometry FROM locationattributes WHERE locationid=",
                                    ws_id, ";"))
-  return(ws_geom)
+  ws_geom_tr= st_transform(ws_geom, crs=st_crs(4326))
+  return(ws_geom_tr)
 }
 
-baseline_geom= grab_geom(161)
-baseline_geomt=st_transform(baseline_geom, crs=st_crs(4326))
-plot(baseline_geomt)
-
-date_seq<-seq(as.Date("2022-10-01"), as.Date("2023-09-30"), by= "day")
-
 #download by largest extent, then use extract w diff watersheds
-grab_ws_swe = function(ws_geom_trs, date_seq){ #need to make work for both single date and sequence
+grab_ws_snow = function(ws_geom_trs, date_seq, param){ #need to make work for both single date and sequence
+  
+  #look in db to see if the metric exists
+    #return values
+  
+  #if doesnt exist in database pull all data for that date / date range & calculate metrics
+  
   
   # pull extent of watershed
   extent=matrix(st_bbox(ws_geom_trs), nrow=2)
@@ -47,13 +48,21 @@ grab_ws_swe = function(ws_geom_trs, date_seq){ #need to make work for both singl
   out_swe<-terra::extract(out_img, baseline_geomt)
   tot_swe<-c(date_ts, sum(out_swe$X2023.03.16)) ## this needs to be modified
   
+  #write new metrics to database
+  #return metric of interest
+  
   return(tot_swe) #consider returning out_img for data vis
 }
 
-yr_out<- grab_ws_swe(baseline_geomt, date_seq)
+# Run functions to download and extract relevant data
+
+date_seq<-seq(as.Date("2021-10-01"), as.Date("2022-09-30"), by= "day")
+full_extent = grab_geom(161)
+
+out<- grab_ws_snow(full_extent, date_seq, param ='SWE')
 
 plot(out_img)
-plot(baseline_geomt, add=TRUE)
+plotfull_extent, add=TRUE)
 
 
 
