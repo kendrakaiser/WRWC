8fa9bfa5143c9f1c060aec4692087cd1517f5873
kendrakaiser <kendra.kaiser@gmail.com>
2023-04-27 16:16:39 -0600

    Cleaned up comments left next steps

--- code/get_snow.R
+++ code/get_snow.R
@@ -9,38 +9,31 @@ library(R.utils)
 library(raster)
 library(sf)
 library(terra)
+library(RPostgres)
 
-#to run on sam's coimputer:
-source("~/Documents/R Workspace/snodasr/SNODASR_functions.R")
+# kek think about other snodas metrics to calculate; confirm IDs of relevant pourpoints; 
+
+#to run on sam's computer:
+#source("~/Documents/R Workspace/snodasr/SNODASR_functions.R")
 
 
 #source("~/github/SNODASR-main/R/extract.SNODAS.subset.R")
 #source("~/github/SNODASR-main/R/download.SNODAS.R")
-#library(SNODASR)
+source(paste0(git_dir,"/code/dbIntakeTools.R")) #tools to connect and write to database
+conn=scdbConnect() #connect to database
 
-#made a note
 
-#df of snodas-derived metric names and units
+#data frame of snodas-derived metric names and units
+# add additional metrics here
 snodasMetrics=data.frame(metric=c("SWE_total"),
                          units=c("cubic meters")
 )
-#each new derived metric must be defined here, and must have a function created to calculate it
-#the function is then matched to the metric in the 'switch' statement in the main function
 
+#### ------------ Define Metrics and associated functions ------------------ ###
+# each new derived metric must be defined here, and must have a function created to calculate it
+# the function is then matched to the metric in the 'switch' statement in the main function
 
-# I added some more functions for easy writing to the db:
-source(paste0(getwd(),"/code/dbIntakeTools.R"))
-conn=scdbConnect()
-#^this^ replaces init_db, but it does pollute the workspace with too many functions.
-# we can hide them if we want as follows:
-.ghost = function(){return("boo")}
-#note that there is no .ghost function in the workspace, but i t still works
-.ghost()
-
-
-#source("~/github/WRWC/code/init_db.R") # /Documents
-
-
+# Extract swe from extent
 extract_ws_swe <- function(ws_id, ws_geoms, date){ 
   # geometries of sub watershed to use to extract metrics of interest
   ws_geom_tr= st_transform(ws_geoms[ws_geoms$outflowlocationid == ws_id,], crs=st_crs(4326))
@@ -49,8 +42,6 @@ extract_ws_swe <- function(ws_id, ws_geoms, date){
   out_img<-extract.SNODAS.subset(date, values_wanted='SWE', extent=ws_extent, write_file = FALSE) 
   #convert images to raster
   out_rast=rast(out_img[[1]])
-  
-  #is there another function to extract data by elevation band??
   
   ## --- extract values from all relevant parameters and modify into metrics ---#
   out_swe<-terra::extract(out_rast, vect(ws_geom_tr))
@@ -61,15 +52,17 @@ extract_ws_swe <- function(ws_id, ws_geoms, date){
   
   tot_swe_m=tot_swe/1000 # convert to meters
   
-  # raster pixels are aproxxamitly 929*673, or more precisley, 625129 meters^2 per pixel on average
-  
+  # raster pixels are approximately 929*673, or more precisely, 625129 meters^2 per pixel on average
   tot_swe_m3=tot_swe_m*625129
-  
+  # add error catch to make sure there is data in here
   return(tot_swe_m3)
 }
 
 # write functions for other metrics of interest
+# e.g. temp function...
 
+### ---------------- Grab and/or Process SNODAS Data ------------------------ ###
+# Wrapper function that pulls data from db if it exists or downloads and processes SNODAS data
 
 grab_ws_snow = function(ws_ids, date, metric, metricDefinitions=snodasMetrics){
   
@@ -100,17 +93,15 @@ grab_ws_snow = function(ws_ids, date, metric, metricDefinitions=snodasMetrics){
                                       paste(ws_ids, collapse= "', '"),"');"))
   
   
-  
-  
-  
-  #since we have downloaded the SNODAS data, calulate all metrics regardless of what was asked for:
+  #since we have downloaded the SNODAS data, calculate all metrics regardless of what was asked for:
   for(addMetric in metricDefinitions$metric){
     
     #simple way to match 'metric' arg to this whole function to a specific function for calculating that metric.  
     #For example, if metric = 'SWE_total', this will run sapply(ws_ids, extract_ws_swe, ws_geoms) , and return the result as SNODAS_values.
     #we need to add more functions for other  metrics of interest, and add the metrics to the check above (knownMetrics)
     SNODAS_values=switch(addMetric,
-                         SWE_total=sapply(ws_ids, extract_ws_swe, ws_geoms=ws_geoms, date=date)
+                         SWE_total=sapply(ws_ids, extract_ws_swe, ws_geoms=ws_geoms, date=date)#,
+                         # insert other metric e.g. temp=sapply(ws_ids, extract_ws_temp, ws_geoms=ws_geoms, date=date)
                          )
     
     
@@ -118,11 +109,10 @@ grab_ws_snow = function(ws_ids, date, metric, metricDefinitions=snodasMetrics){
     dbWriteData(metric = addMetric, value = SNODAS_values, datetime = date, locationID = ws_ids, sourceName = "snodas", units = metricDefinitions$units[metricDefinitions$metric==addMetric],addMetric = T)
   }
   
-  ########################remove snodas source files directory
-  ################ this is consistent with the default in snodasr functions
+  ###---------------- remove snodas source files directory ------------------###
   unlink("./SNODAS",recursive=T)
   
-  #everything has been calculated an written to db.  Return to the users  dataframe to return
+  #everything has been calculated an written to db. Return to the users dataframe
   dbData=dbGetQuery(conn, paste0("SELECT metric, value, datetime, locationid FROM data WHERE data.locationid IN ('",paste(ws_ids,collapse="', '"),"') AND datetime::date = '",date,"' AND metric = '",addMetric,"';"))
   return(dbData) #return the same values that were written to db
 }
@@ -134,11 +124,11 @@ grab_ws_snow = function(ws_ids, date, metric, metricDefinitions=snodasMetrics){
 
 
 #simple test:
-swe_totals=grab_ws_snow(ws_ids = 140, date=as.Date("2023-01-12"),metric="SWE_total")
+swe_totals=grab_ws_snow(ws_ids = 140, date=as.Date("2023-04-12"),metric="SWE_total")
 
 
 #multiple locations
-swe_totals=grab_ws_snow(ws_ids = c(140,167), date=as.Date("2023-01-15"),metric="SWE_total")
+swe_totals=grab_ws_snow(ws_ids = c(140,167), date=as.Date("2023-04-15"),metric="SWE_total")
 
 #grab_ws_snow does not work across dates, but we can make a wrapper for it, or just apply:
 date_seq<-seq(as.Date("2021-10-01"), as.Date("2021-10-11"), by= "day")
