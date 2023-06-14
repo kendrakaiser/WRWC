2ea2efe5fe22e2747e1fd232212f4a11387ac946
Kendra Kaiser <kendrakaiser@boisestate.edu>
2023-06-14 10:10:41 -0600

    pivoted snodas data

--- code/snodas_exploration.R
+++ code/snodas_exploration.R
@@ -4,7 +4,7 @@
 snodas<-read.csv(file.path(data_dir, 'allSnodasData.csv'))
 snotel<-read.csv(file.path(data_dir, 'snotel_data.csv'))
 streamflow<-read.csv(file.path(data_dir, 'streamflow_data.csv'))
-allDat<-read.csv(file.path(data_dir, 'alldat_feb.csv'))
+allDat<-read.csv(file.path(data_dir, 'all_dat_apr.csv'))
 
 #modify df to subset and plot
 snodas$year <- year(snodas$datetime)
@@ -14,21 +14,40 @@ snodas$day<- day(snodas$datetime)
 vol <- allDat %>% dplyr ::select (year, bwb.vol, bws.vol, cc.vol, sc.vol) %>% pivot_longer(cols=c('bwb.vol', 'bws.vol', 'cc.vol', 'sc.vol'), names_to = 'site', values_to = 'volume')
 vol<- vol[!is.na(vol$volume),]
 vol<- vol[vol$volume>0,]
-siteIDs<-t(matrix(c(140, 'BIG WOOD RIVER AT HAILEY', 'bwb.vol',167,'CAMAS CREEK NR BLAINE ID', 'cc.vol',  144, 'SILVER CREEK AT SPORTSMAN ACCESS', 'sc.vol', 141, 'BIG WOOD RIVER AT STANTON CROSSING', 'bws.vol'), nrow=3, ncol=4))
-colnames(siteIDs)<-c('locationid', 'name', 'site')
+siteIDs<-t(matrix(c(140, 'BIG WOOD RIVER AT HAILEY', 'bwb.vol', 'bwb', 167,'CAMAS CREEK NR BLAINE ID', 'cc.vol', 'cc', 144, 'SILVER CREEK AT SPORTSMAN ACCESS', 'sc.vol', 'sc', 141, 'BIG WOOD RIVER AT STANTON CROSSING', 'bws.vol', 'bws'), nrow=4, ncol=4))
+colnames(siteIDs)<-c('locationid', 'name', 'site', 'site.s')
 vol<-merge(vol, siteIDs, by='site')
+
+
+sno.wide<- snodas[,c(1:3,5)] %>% pivot_wider(names_from = c(metric, locationid), names_glue = "{metric}.{locationid}", values_from = value) 
+allDat<- merge(allDat, siteIDs, by='site')
+
 
 #subset timeseries data to plot 
 sno_april1<- snodas[snodas$mo == 4 & snodas$day ==1,] %>% dplyr::select (metric, value, locationid,year)
 
 snovol<- merge(vol,sno_april1, by=c('year', 'locationid'))
 
+#addnl subsetting, change to tidy verse or cleaner way, there is a prob w snodas data having two versions of swe 
 snovol.swe<- subset(snovol, metric=='swe_total' & site != 'bws.vol' & site != 'sc.vol')
-#filter(metric=='swe_total') %>% filter(site )
-snovol.bws<- subset(snovol, metric=='SWE_total')
+snovol.bws<- subset(snovol, metric=='swe_total' & site == 'bws.vol')
+snovol.sca<- subset(snovol, metric=='snow_covered_area')# & site != 'bws.vol')
+snovol.runoff<- subset(snovol, metric=='runoff_total' & site != 'bws.vol')
 
-ggplot(snovol.swe, aes(x=value, y=volume, color=site)) + geom_point() + 
-  theme_bw()
+# plot swe versus total summer volume
+ggplot(snovol.swe, aes(x=value, y=volume/1000, color=site)) + geom_point() + 
+  theme_bw() +ylab('Irrigation Season Vol KAF') +xlab('SWE')
+  # plot SNOTEL data over top of this 
+  
+ggplot(snovol.sca, aes(x=value, y=volume/1000, color=site)) + geom_point() + 
+  theme_bw() +ylab('Irrigation Season Vol KAF')  +xlab('Snow Covered Area')
 
-ggplot(snovol.bws, aes(x=value, y=volume)) + geom_point() + 
-  theme_bw()
+#runoff needs to be cumulative? does it actually makes sense?
+ggplot(snovol.runoff, aes(x=value, y=volume/1000, color=site)) + geom_point() + 
+  theme_bw() +ylab('Irrigation Season Vol KAF')  +xlab('Runoff')
+
+ggplot(snovol.bws, aes(x=value, y=volume/1000)) + geom_point() + 
+  theme_bw()+ylab('Irrigation Season Vol KAF')  +xlab('Snow Covered Area')
+
+
+sno.runoff<- subset(snodas, metric=='runoff_total' & locationid == 140)
