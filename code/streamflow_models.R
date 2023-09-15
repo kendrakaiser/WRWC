cf56f7fd51f86918695187decad27f09c4165560
Kendra Kaiser <kendrakaiser@boisestate.edu>
2023-09-15 12:48:52 -0600

    moved data meangemtn into seperate script

--- code/streamflow_models.R
+++ code/streamflow_models.R
@@ -11,41 +11,15 @@
 defaultW <- getOption("warn") 
 options(warn = -1) 
 
-# Import & Compile Data -------------------------------------------------------# 
-# Streamflow, Current SWE, historic and Modeled Temperature Data
-#q = read.csv(file.path(cd,'streamflow_data.csv'))
-usgs_sites = read.csv(file.path(data_dir,'usgs_sites.csv'))
-stream.id<-unique(as.character(usgs_sites$abv))
-
-swe_q = read.csv(file.path(data_dir,input_data))
-swe_q$bws.vol[swe_q$year <=1996]<- NA
-swe_q[swe_q == 0] <- 0.01 # change zeros to a value so lm works 
-swe_q<-swe_q[!(names(swe_q) %in% c("bwb.cm.nat","bws.cm.nat","bwb.vol.nat","bws.vol.nat","bws.loss","sc.vol.nat"))]
-
-spring.temps = read.csv(file.path(data_dir, 'sprTemps.csv'))
-nj.temps = read.csv(file.path(data_dir, 'njTemps.csv'))
-# var<- read.csv(file.path(model_out,'all_vars.csv'))
-var = swe_q %>% inner_join(spring.temps, by ="year") %>% inner_join(nj.temps, by ="year")
-# sp.test<-apply(var, MARGIN=2, shapiro.test) 
-#TODO : automate this step (e.g. if p < 0.05 log it and remove the og)
-# normailze parameters that have a shapiro.test() < 0.05
-var$log.cg.swe <- log(var$cg.swe)
-var$log.g.swe <- log(var$g.swe)
-var$log.gs.swe <- log(var$gs.swe)
-var$log.hc.swe <- log(var$hc.swe)
-var$log.lwd.swe <- log(var$lwd.swe)
-var$log.ga.swe <- log(var$ga.swe)
-var$log.bc.swe <- log(var$bc.swe)
-var<-var[,!(names(var) %in% c('cg.swe', 'g.swe','gs.swe','hc.swe', 'lwd.swe','ga.swe','bc.swe', 'nj.t.sr', 'aj.t.sr'))]
-
-#save variables for use in other scripts
-write.csv(var, file.path(model_out,'all_vars.csv'), row.names = FALSE)
+#------------------------------------------------------------------------------ # 
+# Import & Compile Data                                                        -# 
+#------------------------------------------------------------------------------ # 
+var<- read.csv(var, file.path(model_out,'all_vars.csv'), row.names = FALSE)
 
 swe_cols<-grep('swe', colnames(var))
 t_cols<-grep('.t.', colnames(var))
 wint_t_cols<-grep('nj.t', colnames(var))
 vol_cols<- grep('vol', colnames(var))
-#snodas_cols<- c(grep('liquid', colnames(var)), grep('runoff', colnames(var)), grep('snow', colnames(var)), grep('swe_total', colnames(var)))
 snodas_cols<- c(grep('wint', colnames(var)), grep('runoff', colnames(var)), grep('snow', colnames(var)), grep('swe_total', colnames(var)))
 
 #par(mar=c(1, 1, 1, 1))
