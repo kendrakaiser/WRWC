e1b1dc7577f5485f64438b8c0f20066d68d1bcba
Kendra Kaiser <kendrakaiser@boisestate.edu>
2023-09-15 13:14:31 -0600

    added all data call and clean scripts to the beginning

--- code/run_models.R
+++ code/run_models.R
@@ -41,7 +41,7 @@ writeLines(capture.output(sessionInfo()), file.path(cd, "sessionInfo.txt"))
 source(file.path(git_dir, 'code/data_scraping.R'))
 source(file.path(git_dir, 'code/download_agrimet.R'))
 source(file.path(git_dir, 'code/temperature_models.R')) #fix - fig_dir_mo
-source(file.path(git_dir, 'code/get_snow.R')) # download SNODAS from db
+source(file.path(git_dir, 'code/get_snow.R')) # TODO: change this; download SNODAS from db
 source(file.path(git_dir, 'code/snodas_exploration.R')) # make figures and compile snodas data
 source(file.path(git_dir, 'code/data_integration.R'))  # compiles data into one matrix TODO: modify to clean up above workflow
 
