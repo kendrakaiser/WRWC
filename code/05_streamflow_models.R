65ef99cf6e03a513185cc1d537d9ab94d0d80c24
Kendra Kaiser <kendrakaiser@boisestate.edu>
2023-10-26 15:58:50 -0600

    updated intro text

--- code/05_streamflow_models.R
+++ code/05_streamflow_models.R
@@ -1,11 +1,11 @@
 # ----------------------------------------------------------------------------- #
-# Predictive Streamflow Model Exploration for the Wood River Water Collaborative
+# Predictive Streamflow Model Selection for the Wood River Water Collaborative
 # Kendra Kaiser
 # October 1, 2020
 #
-# Linear models to predict total irrigation season streamflow volume (Acre-Feet)  
-# and center of mass based on calculated baseflow, current SWE, winter and 
-# predicted spring temperature 
+# Multiple linear regression models to predict total irrigation season streamflow 
+# volume (Acre-Feet) and center of mass based on calculated baseflow, current SWE, 
+# winter and predicted spring temperature 
 # ----------------------------------------------------------------------------- # 
 
 defaultW <- getOption("warn") 
