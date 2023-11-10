2769a7a3d28a46b8b59fc2cf21cec76e25792b56
Kendra Kaiser <kendra.kaiser@gmail.com>
2023-11-10 14:13:44 -0700

    moved boxplots over to figure generation

--- code/10_figure_gen.R
+++ code/10_figure_gen.R
@@ -1,6 +1,140 @@
-# Figures for data analysis and model output from wood river streamflow florecasting
+# Figures and tables for data analysis and model output from wood river streamflow forecasting
+
+# ------------------------------------------------------------------------------
+# Calculate Exceedance Probabilities
+# ------------------------------------------------------------------------------
+
+exceed.probs<- function(vols, probs){
+  'calculate exceedance probabilities of model output
+  p=m/(n+1)
+  vols: numeric of volumes
+  probs: array of probabilities to calculate'
+  
+  n=length(vols) 
+  m=round(probs*(n+1))
+  #rank the sampled volumes
+  ranks<- rank(vols)
+  # find the index of which volume goes with each exceedance
+  ix=match(m, ranks)
+  # find the actual volume of each exceedance
+  ex.vols=exp(vols[ix])
+  return(ex.vols)
+}
+
+# Exceedance probs from NWRFC
+prb<- c(0.1, 0.25, 0.5, 0.75, 0.9)
+# Calculate exceedance probabilities and save table with labels
+ex.vols<- round(apply(vol.sample, 2, exceed.probs, prb)/1000) %>% as.data.frame()
+ex.vols$Exceedance <- c('90%', '75%', '50%', '25%', '10%') 
+ex.vols<- ex.vols%>% relocate(Exceedance)
+
+my_table_theme <- ttheme_default(core=list(fg_params = list(col = c("red","darkorange","green3","deepskyblue", "blue3"), col=NA)))
+png(file.path(fig_dir_mo,"ex.vols.png"), height = 30*nrow(ex.vols), width = 130*ncol(ex.vols))
+grid.table(ex.vols, theme = my_table_theme, rows = NULL)
+dev.off()
+
+#saveRDS(round(ex.vols,0), file.path(fig_dir_mo,"ex.vols.rds"))
+ex.vols3 <- ex.vols %>%pivot_longer(!Exceedance, names_to="site", values_to="value")
+ex.vols3$t<- "Predicted"
+
+# ------------------------------------------------------------------------------
+# Plot boxplots of total annual flow from each model
+# ------------------------------------------------------------------------------
+# Subset for plotting
+vol.hist<- as.data.frame(var[var$year < pred.yr ,] %>% dplyr::select(c(bwh.vol, bws.vol, cc.vol)) %>% `colnames<-`(c("Big Wood Hailey Hist", "Big Wood Stanton Hist","Camas Creek Hist")) %>%pivot_longer(everything(),  names_to = "site", values_to = "value") )
+vol.hist$value<-vol.hist$value/1000
+vol.hist$t<- "Historic"
+vol.hist.sm<-as.data.frame(var[var$year < pred.yr,] %>% dplyr::select(c(sc.vol)) %>% `colnames<-`(c("Silver Creek Hist")) %>% pivot_longer(everything(),  names_to = "site", values_to = "value") )
+vol.hist.sm$value<-vol.hist.sm$value/1000
+vol.hist.sm$t<- "Historic"
+
+vol.pred <-as.data.frame(exp(vol.sample)/1000) %>% pivot_longer(everything(),  names_to = "site", values_to = "value")
+vol.pred$t<- "Predicted"
+
+vol.big<- rbind(vol.hist, vol.pred[vol.pred$site != "Silver Creek",])
+vol.sm<- rbind(vol.hist.sm, vol.pred[vol.pred$site == "Silver Creek",])
+vol.cc<- rbind(vol.hist[vol.hist$site == "Camas Creek Hist",], vol.pred[vol.pred$site == "Camas Creek",])
+vol.cc$t <- factor(vol.cc$t)
+vol.big$t <- factor(vol.big$t)
+vol.sm$t <- factor(vol.sm$t)
+vol.big$site<-factor(vol.big$site,levels = c("Big Wood Hailey Hist","Big Wood Hailey", "Big Wood Stanton Hist", "Big Wood Stanton", "Camas Creek Hist", "Camas Creek" ), ordered = TRUE)
+vol.sm$site<-factor(vol.sm$site,levels = c("Silver Creek Hist","Silver Creek"), ordered = TRUE)
+vol.cc$site<-factor(vol.cc$site,levels = c("Camas Creek Hist", "Camas Creek" ), ordered = TRUE)
 
 
+colfunc<-colorRampPalette(c("red","darkorange","green3","deepskyblue", "blue3"))
+
+# Plot boxplots of total annual flow from each model with exceedance probabilities
+
+p<-ggplot(vol.big, aes(x=site, y=value, fill=t), alpha=0.6) +
+  geom_boxplot(outlier.alpha = 0.3) +
+  scale_fill_manual(values=c("royalblue3", "grey90")) +
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 10))+
+  scale_y_continuous(breaks = round(seq(0, max(vol.big$value, na.rm=TRUE), by = 50),1))+
+  
+  geom_point(data=ex.vols3[ex.vols3$site !="Silver Creek",], aes(x=site, y=value, color=as.factor(Exceedance)), size=2, shape=15)+
+  scale_color_manual(values=c("blue3", "deepskyblue", "green3","darkorange","red"))+
+  theme_bw(base_size = 14)+
+  ggtitle("Historic & Modeled Irrigation Season Volumes (April-Sept.)") +
+  labs(fill="", color="Exceedance")+
+  xlab("")+
+  ylab("Irrigation Season Volume (KAF)") 
+
+png(filename = file.path(fig_dir_mo,"sampled_volumes.png"),
+    width = 6.5, height = 5.5,units = "in", pointsize = 12,
+    bg = "white", res = 600) 
+print(p)
+dev.off()
+
+saveRDS(p, file.path(fig_dir_mo,"sampled_volumes.rds"))
+
+ps<- ggplot(vol.sm, aes(x=site, y=value, fill=site), alpha=0.6) +
+  geom_boxplot(outlier.alpha = 0.3) +
+  scale_fill_manual(values=c("royalblue3", "grey90")) +
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 10))+
+  scale_y_continuous(breaks = round(seq(0, max(vol.sm$value, na.rm=TRUE), by = 10),1))+
+  geom_point(data=ex.vols3[ex.vols3$site =="Silver Creek",], aes(x=site, y=value, color=as.factor(Exceedance)), size=2, shape=15)+
+  scale_color_manual(values=c("blue3", "deepskyblue", "green3","darkorange","red"))+
+  
+  theme_bw(base_size = 18)+
+  theme(legend.position="none") +
+  ggtitle("") +
+  labs(fill="", color="Exceedance")+
+  xlab("")+
+  ylab("Irrigation Volume (KAF)")
+
+png(filename = file.path(fig_dir_mo,"sampled_sc_vol.png"),
+    width = 5.5, height = 5.5,units = "in", pointsize = 12,
+    bg = "white", res = 600) 
+print(ps)
+dev.off()
+
+saveRDS(ps, file = file.path(fig_dir_mo, paste0("sampled_sc_vol-", end_date, ".rds")))
+
+pc<- ggplot(vol.cc, aes(x=site, y=value, fill=site), alpha=0.6) +
+  geom_boxplot(outlier.alpha = 0.3) +
+  scale_fill_manual(values=c("royalblue3", "grey90")) +
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 10))+
+  scale_y_continuous(breaks = round(seq(0, max(vol.cc$value, na.rm=TRUE), by = 20),1))+
+  geom_point(data=ex.vols3[ex.vols3$site =="Camas Creek",], aes(x=site, y=value, color=as.factor(Exceedance)), size=2, shape=15)+
+  scale_color_manual(values=c("red","darkorange","green3","deepskyblue", "blue3"))+
+  
+  theme_bw()+
+  theme(legend.position="none") +
+  ggtitle("") +
+  labs(fill="", color="Exceedance")+
+  xlab("")+
+  ylab("Irrigation Volume (KAF)")
+
+png(filename = file.path(fig_dir_mo,"sampled_cc_vol.png"),
+    width = 5.5, height = 5.5,units = "in", pointsize = 12,
+    bg = "white", res = 600) 
+print(pc)
+dev.off()
+
+
+
+#-------------------------------------------------------------------------------
 ### Streamflow Simulation Figures
 #-------------------------------------------------------------------------------
 
@@ -99,3 +233,7 @@ lines(dates,bwh.wy$value[bwh.wy$wy == 2014][183:365], lwd=1, col="black")
 lines(dates,bwh.wy$value[bwh.wy$wy == 2013][183:365], lwd=1, col="black")
 lines(dates,bwh.wy$value[bwh.wy$wy == 2019][183:365], lwd=1, col="blue")
 lines(dates,bwh.wy$value[bwh.wy$wy == 1998][183:365], lwd=1, col="red")
+
+
+#matplot(bwh.flow.s, type='l',col = "gray90" )
+#matplot(sc.flow.s, type='l',col = "gray90" )
