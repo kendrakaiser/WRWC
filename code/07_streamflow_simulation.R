97cd6f337b4779b88870dd2654fcf1685fcd652f
Kendra Kaiser <kendra.kaiser@gmail.com>
2023-11-10 14:01:40 -0700

    updated naming conventions throughout

--- code/07_streamflow_simulation.R
+++ code/07_streamflow_simulation.R
@@ -98,18 +98,17 @@ pi[,9:12]<-as.data.frame(pred.int(sc.flow.s))
 pi[,13:16]<-as.data.frame(pred.int(cc.flow.s))
 
 ### Average Annual streamflow with maximum and minimum daily flows
-q = read.csv(file.path(data_dir,'streamflow_data.csv'))
-q$Date <- as.Date(q$Date)
-q$doy <- yday(q$Date)
-q<- q %>% filter(abv != 'bwr')
+streamflow_data$Date <- as.Date(streamflow_data$datetime)
+streamflow_data$doy <- yday(streamflow_data$Date)
+
 
 #Calculate day of water-year
 water_year_begin <- ymd('1987-10-01')-1
 #deal with leap years
-q$doWY<- ((q$doy - yday(water_year_begin)) %% ifelse(leap_year(year(q$Date)), 366, 365)) +1
+streamflow_data$doWY<- ((streamflow_data$doy - yday(water_year_begin)) %% ifelse(leap_year(year(streamflow_data$Date)), 366, 365)) +1
 
 
-data <- q %>% filter(abv == 'bwh') %>% group_by(doWY) %>% dplyr::mutate(meanQ=mean(Flow, na.rm=TRUE))
+data <- streamflow_data %>% filter(abv == 'bwh') %>% group_by(doWY) %>% dplyr::mutate(meanQ=mean(value, na.rm=TRUE))
 
 minBW=min(pi[,1], pi[,5],data$meanQ[457:639])
 maxBW=max(pi[,2], pi[,6],data$meanQ[457:639])
@@ -118,7 +117,7 @@ png(filename = file.path(fig_dir_mo, "bwh_Simulation.png"),
     width = 5.5, height = 5.5,units = "in", pointsize = 12,
     bg = "white", res = 600) 
 plot(dates, pi[,3], type="n", xlab="Date", ylab ="Flow (cfs)",
-     main = "Big Wood Natural Streamflow at Hailey", ylim=c(minBW, maxBW))
+     main = "Big Wood River Streamflow at Hailey", ylim=c(minBW, maxBW))
 polygon(c(dates[1], dates, rev(dates)), c(pi[1,1], pi[,2], rev(pi[,1])), 
         col = "gray90", border = NA)
 lines(dates,data$meanQ[457:639],lwd=1.5,col="black")
@@ -129,8 +128,8 @@ legend("topright", inset=.02, legend=c("Historic Avg.", "Avg Simulation"),
 #lines(dates,flow[183:365], lwd=2, col="green")
 dev.off()
 
-# Summary stats
-data <- q %>% filter(abv == 'bws') %>% group_by(doWY) %>% dplyr::mutate(meanQ=mean(Flow, na.rm=TRUE))
+# Big Wood @ Stanton
+data <- streamflow_data %>% filter(abv == 'bws') %>% group_by(doWY) %>% dplyr::mutate(meanQ=mean(value, na.rm=TRUE))
 # Big Wood @ Stanton
 png(filename = file.path(fig_dir_mo, "BWS_Simulation.png"),
     width = 5.5, height = 5.5,units = "in", pointsize = 12,
@@ -147,15 +146,15 @@ legend("topright", inset=.02, legend=c("Historic Avg.", "Avg Simulation"),
 #lines(dates,flow[183:365], lwd=2, col="green")
 dev.off()
 
-# Summary stats
-data <- q %>% filter(abv == 'sc') %>% group_by(doWY) %>% dplyr::mutate(meanQ=mean(Flow, na.rm=TRUE))
+# Silver Creek
+data <- streamflow_data %>% filter(abv == 'sc') %>% group_by(doWY) %>% dplyr::mutate(meanQ=mean(value, na.rm=TRUE))
 
 # Silver Creek
 png(filename = file.path(fig_dir_mo, "SC_Simulation.png"),
     width = 5.5, height = 5.5,units = "in", pointsize = 12,
     bg = "white", res = 600) 
 plot(dates, pi[,11], type="n", xlab="Date", ylab ="Flow (cfs)",
-     main = "Silver Creek Natural Streamflow Simulation", ylim=c(min(pi[,9]), max(pi[,10])))
+     main = "Silver Creek Streamflow Simulation", ylim=c(min(pi[,9]), max(pi[,10])))
 polygon(c(dates[1], dates, rev(dates) ), c(pi[1,9], pi[,10], rev(pi[,9])), 
         col = "gray90", border = NA)
 lines(dates,data$meanQ[91:273],lwd=1.5,col="black")
@@ -166,8 +165,9 @@ legend("topright", inset=.02, legend=c("Historic Avg.", "Avg Simulation"),
 #lines(dates,flow[183:365], lwd=2, col="green")
 dev.off()
 
-# Summary stats 
-data <- q %>% filter(abv == 'cc') %>% group_by(doWY) %>% dplyr::mutate(meanQ=mean(Flow))
+# Camas Creek
+
+data <- streamflow_data %>% filter(abv == 'cc') %>% group_by(doWY) %>% dplyr::mutate(meanQ=mean(value, na.rm=TRUE))
 mincc=min(pi[,13], data$meanQ[91:273])
 maxcc=max(pi[,14], data$meanQ[91:273])
 # Camas Creek
