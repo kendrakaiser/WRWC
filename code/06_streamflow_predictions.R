1e18796e5c8217f51fe243349a38f89adb04f03a
Kendra Kaiser <kendra.kaiser@gmail.com>
2023-11-10 14:19:14 -0700

    moved figures out of script, fixed CM naming convention and left note

--- code/06_streamflow_predictions.R
+++ code/06_streamflow_predictions.R
@@ -314,7 +314,7 @@ flow.data = var[var$year >= 1997 & var$year < 2022,] %>% dplyr::select(bwh.vol,
 
 # calculate correlations between gages' total volume, diversions and center of mass
 cor.mat<-cor(cbind(flow.data[c(1,3,5,7)],flow.data[c(2,4,6,8)]),use="pairwise.complete")
-#pred.params.vol[2,2]<-0.2
+
 # create covariance matrix by multiplying by each models standard error
 pred.pars<-rbind(pred.params.vol[,1:2], pred.params.cm)
 outer.prod<-as.matrix(pred.pars[,2])%*%t(as.matrix(pred.pars[,2]))
@@ -334,137 +334,6 @@ dev.off()
 # save output from correlations
 write.csv(cov.mat, file.path(model_out,"cov.mat.csv"),row.names=T)
 write.csv(pred.pars, file.path(model_out,"pred.pars.csv"),row.names=T)
-
-# ------------------------------------------------------------------------------
-# Calculate Exceedance Probabilities
-# ------------------------------------------------------------------------------
-
-exceed.probs<- function(vols, probs){
-  'calculate exceedance probabilities of model output
-  p=m/(n+1)
-  vols: numeric of volumes
-  probs: array of probabilities to calculate'
-  
-  n=length(vols) 
-  m=round(probs*(n+1))
-  #rank the sampled volumes
-  ranks<- rank(vols)
-  # find the index of which volume goes with each exceedance
-  ix=match(m, ranks)
-  # find the actual volume of each exceedance
-  ex.vols=exp(vols[ix])
-  return(ex.vols)
-}
-
-# Exceedance probs from NWRFC
-prb<- c(0.1, 0.25, 0.5, 0.75, 0.9)
-# Calculate exceedance probabilities and save table with labels
-ex.vols<- round(apply(vol.sample, 2, exceed.probs, prb)/1000) %>% as.data.frame()
-ex.vols$Exceedance <- c('90%', '75%', '50%', '25%', '10%') 
-ex.vols<- ex.vols%>% relocate(Exceedance)
-
-my_table_theme <- ttheme_default(core=list(fg_params = list(col = c("red","darkorange","green3","deepskyblue", "blue3"), col=NA)))
-png(file.path(fig_dir_mo,"ex.vols.png"), height = 30*nrow(ex.vols), width = 130*ncol(ex.vols))
-grid.table(ex.vols, theme = my_table_theme, rows = NULL)
-dev.off()
-
-#saveRDS(round(ex.vols,0), file.path(fig_dir_mo,"ex.vols.rds"))
-ex.vols3 <- ex.vols %>%pivot_longer(!Exceedance, names_to="site", values_to="value")
-ex.vols3$t<- "Predicted"
-# ------------------------------------------------------------------------------
-# Plot boxplots of total annual flow from each model
-# ------------------------------------------------------------------------------
-# Subset for plotting
-vol.hist<- as.data.frame(var[var$year < 2020,] %>% dplyr::select(c(bwh.vol, bws.vol, cc.vol)) %>% `colnames<-`(c("Big Wood Hailey Hist", "Big Wood Stanton Hist","Camas Creek Hist")) %>%pivot_longer(everything(),  names_to = "site", values_to = "value") )
-vol.hist$value<-vol.hist$value/1000
-vol.hist$t<- "Historic"
-vol.hist.sm<-as.data.frame(var[var$year < 2020,] %>% dplyr::select(c(sc.vol)) %>% `colnames<-`(c("Silver Creek Hist")) %>% pivot_longer(everything(),  names_to = "site", values_to = "value") )
-vol.hist.sm$value<-vol.hist.sm$value/1000
-vol.hist.sm$t<- "Historic"
-
-vol.pred <-as.data.frame(exp(vol.sample)/1000) %>% pivot_longer(everything(),  names_to = "site", values_to = "value")
-vol.pred$t<- "Predicted"
-
-vol.big<- rbind(vol.hist, vol.pred[vol.pred$site != "Silver Creek",])
-vol.sm<- rbind(vol.hist.sm, vol.pred[vol.pred$site == "Silver Creek",])
-vol.cc<- rbind(vol.hist[vol.hist$site == "Camas Creek Hist",], vol.pred[vol.pred$site == "Camas Creek",])
-vol.cc$t <- factor(vol.cc$t)
-vol.big$t <- factor(vol.big$t)
-vol.sm$t <- factor(vol.sm$t)
-vol.big$site<-factor(vol.big$site,levels = c("Big Wood Hailey Hist","Big Wood Hailey", "Big Wood Stanton Hist", "Big Wood Stanton", "Camas Creek Hist", "Camas Creek" ), ordered = TRUE)
-vol.sm$site<-factor(vol.sm$site,levels = c("Silver Creek Hist","Silver Creek"), ordered = TRUE)
-vol.cc$site<-factor(vol.cc$site,levels = c("Camas Creek Hist", "Camas Creek" ), ordered = TRUE)
-
-
-colfunc<-colorRampPalette(c("red","darkorange","green3","deepskyblue", "blue3"))
-
-# Plot boxplots of total annual flow from each model with exceedance probabilities
-  
-p<-ggplot(vol.big, aes(x=site, y=value, fill=t), alpha=0.6) +
-  geom_boxplot(outlier.alpha = 0.3) +
-  scale_fill_manual(values=c("royalblue3", "grey90")) +
-  scale_x_discrete(labels = function(x) str_wrap(x, width = 10))+
-  scale_y_continuous(breaks = round(seq(0, max(vol.big$value, na.rm=TRUE), by = 50),1))+
-
-  geom_point(data=ex.vols3[ex.vols3$site !="Silver Creek",], aes(x=site, y=value, color=as.factor(Exceedance)), size=2, shape=15)+
-  scale_color_manual(values=c("blue3", "deepskyblue", "green3","darkorange","red"))+
-  theme_bw(base_size = 14)+
-  ggtitle("Historic & Modeled Irrigation Season Volumes (April-Sept.)") +
-  labs(fill="", color="Exceedance")+
-  xlab("")+
-  ylab("Irrigation Season Volume (KAF)") 
-
-png(filename = file.path(fig_dir_mo,"sampled_volumes.png"),
-    width = 6.5, height = 5.5,units = "in", pointsize = 12,
-    bg = "white", res = 600) 
-print(p)
-dev.off()
-
-saveRDS(p, file.path(fig_dir_mo,"sampled_volumes.rds"))
-
-ps<- ggplot(vol.sm, aes(x=site, y=value, fill=site), alpha=0.6) +
-  geom_boxplot(outlier.alpha = 0.3) +
-  scale_fill_manual(values=c("royalblue3", "grey90")) +
-  scale_x_discrete(labels = function(x) str_wrap(x, width = 10))+
-  scale_y_continuous(breaks = round(seq(0, max(vol.sm$value, na.rm=TRUE), by = 10),1))+
-  geom_point(data=ex.vols3[ex.vols3$site =="Silver Creek",], aes(x=site, y=value, color=as.factor(Exceedance)), size=2, shape=15)+
-  scale_color_manual(values=c("blue3", "deepskyblue", "green3","darkorange","red"))+
-  
-  theme_bw(base_size = 18)+
-  theme(legend.position="none") +
-  ggtitle("") +
-  labs(fill="", color="Exceedance")+
-  xlab("")+
-  ylab("Irrigation Volume (KAF)")
-
-png(filename = file.path(fig_dir_mo,"sampled_sc_vol.png"),
-    width = 5.5, height = 5.5,units = "in", pointsize = 12,
-    bg = "white", res = 600) 
-print(ps)
-dev.off()
-
-saveRDS(ps, file = file.path(fig_dir_mo, paste0("sampled_sc_vol-", end_date, ".rds")))
-
-pc<- ggplot(vol.cc, aes(x=site, y=value, fill=site), alpha=0.6) +
-  geom_boxplot(outlier.alpha = 0.3) +
-  scale_fill_manual(values=c("royalblue3", "grey90")) +
-  scale_x_discrete(labels = function(x) str_wrap(x, width = 10))+
-  scale_y_continuous(breaks = round(seq(0, max(vol.cc$value, na.rm=TRUE), by = 20),1))+
-  geom_point(data=ex.vols3[ex.vols3$site =="Camas Creek",], aes(x=site, y=value, color=as.factor(Exceedance)), size=2, shape=15)+
-  scale_color_manual(values=c("red","darkorange","green3","deepskyblue", "blue3"))+
-  
-  theme_bw()+
-  theme(legend.position="none") +
-  ggtitle("") +
-  labs(fill="", color="Exceedance")+
-  xlab("")+
-  ylab("Irrigation Volume (KAF)")
-
-png(filename = file.path(fig_dir_mo,"sampled_cc_vol.png"),
-    width = 5.5, height = 5.5,units = "in", pointsize = 12,
-    bg = "white", res = 600) 
-print(pc)
-dev.off()
 
 
 # ------------------------------------------------------------------------------
@@ -486,9 +355,9 @@ for(i in 1:dim(cm.data)[1]){
   }
 
 # create normal distribution of years 
-CMyear.sample.prob<-sample(cm.data$year,5000,replace=TRUE, prob=cm.data$prob) 
+CMyear.sample<-sample(cm.data$year,5000,replace=TRUE, prob=cm.data$prob) 
 # Of the 5000 replicates show the percentage that each year represents
-cm_prob<-as.data.frame(summary(as.factor(CMyear.sample.prob))/5000)
+cm_prob<-as.data.frame(summary(as.factor(CMyear.sample))/5000)
 colnames(cm_prob)<- c("% of sample")
 cm_prob<- cm_prob*100
 png(file.path(fig_dir_mo,"CM_summary_prob.png"), height = 50*nrow(cm_prob), width = 200*ncol(cm_prob))
@@ -498,6 +367,8 @@ dev.off()
 #save the probabilities for use in the simulation
 write.csv(CMyear.sample, file.path(model_out, paste0("CMyear.sample-", end_date,".csv")),row.names=F)
 
+
+# TESTING
 # Draw sample of years with similar volume for comparison -- 
 vol.data = var[var$year >1996 & var$year < pred.yr,]%>% dplyr::select(year, bwh.vol, bws.vol, cc.vol, sc.vol) 
 vol.data$prob<-NA
@@ -505,6 +376,7 @@ vol.data$prob<-NA
 #TODO: Write this up in a methods section
 samp.sd<- c(sd(log(var$bwh.vol)), sd(log(var$bws.vol)), sd(log(var$cc.vol)), sd(log(var$sc.vol)))
 var.fore<- pred.params.vol[,2] + samp.sd
+
 # pmvnorm calculates the distribution function of the multivariate normal distribution
 for(i in 1:dim(vol.data)[1]){
     vec<-log(vol.data[i,2:5])
@@ -515,14 +387,11 @@ for(i in 1:dim(vol.data)[1]){
     vol.diff<- rbind(vol.diff, exp(as.numeric(vec+var.fore)) - exp(as.numeric(vec-j)))
 }
 
-
-vol_prob<-as.data.frame(summary(as.factor(vol.sample.prob))/5000)*100
+# this part is broken
+vol_prob<-as.data.frame(summary(as.factor(vol.sample))/5000)*100
 colnames(vol_prob)<- c("% of sample")
 
 png(file.path(fig_dir_mo,"vol_prob.png"), height = 50*nrow(vol_prob), width = 200*ncol(vol_prob))
 grid.table(vol_prob)
 dev.off()
 
-#TODO: fix this to save properly
-#save the probabilities for use in the simulation
-#write.csv(CMyear.sample, file.path(model_out, paste0("CMyear.sample-", end_date,".csv")),row.names=F)
