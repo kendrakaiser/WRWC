3af4ac0214141aa7f8602d5237f1720404c73390
Kendra Kaiser <kendra.kaiser@gmail.com>
2023-12-16 18:37:06 -0700

    updated and left notes

--- code/04_data_integration.R
+++ code/04_data_integration.R
@@ -11,11 +11,10 @@ swe_q = read.csv(file.path(data_dir,input_data))
 swe_q[swe_q == 0] <- 0.01 # change zeros to a value so lm works 
 
 # observed temperatures
-spring.temps = read.csv(file.path(data_dir, 'sprTemps.csv'))
-nj.temps = read.csv(file.path(data_dir, 'njTemps.csv'))
+temps = read.csv(file.path(data_dir, "temp_dat.csv"))
 
 # combine discharge & SWE with temp data
-var = swe_q %>% inner_join(spring.temps, by ="wateryear") %>% inner_join(nj.temps, by ="wateryear")
+var = swe_q %>% inner_join(temps, by ="wateryear")
 
 # sp.test<-apply(var, MARGIN=2, shapiro.test) 
 #TODO : automate this step (e.g. if p < 0.05 log it and remove the og)
@@ -30,5 +29,5 @@ var$bc.log_swe <- log(var$bc.swe)
 var<-var[,!(names(var) %in% c('cg.swe', 'g.swe','gs.swe','hc.swe', 'lwd.swe','ga.swe','bc.swe', 'sr.nj_t', 'sr.aj_t'))]
 
 #save variables for use in other scripts
-#TODO: change folder structure so that they are either pre-defined or created
+#TODO: change folder structure so that they are either pre-defined or created -- alt to check w sam about -- remove saving all together because they will stay in the environment?
 write.csv(var, file.path(model_out,'all_vars.csv'), row.names = FALSE)
