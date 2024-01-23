dbd7c1ad198db00111bfb97fb7e7da215b29f880
Kendra Kaiser <kendra.kaiser@gmail.com>
2024-01-23 12:05:21 -0700

    added if statement to determine input data removed local data read

--- code/04_data_integration.R
+++ code/04_data_integration.R
@@ -7,14 +7,21 @@
 # Streamflow, Current SWE, historic Temperature Data
 
 # input data name 'alldat_mo.csv'
-swe_q = read.csv(file.path(data_dir,input_data))
+if (run_date == 'feb1'){
+  swe_q  <<- alldat_feb
+} else if (run_date == 'march1'){
+  swe_q  <<- alldat_mar
+} else if (run_date == 'april1'){
+  swe_q  <<- alldat_april
+}
+
 swe_q[swe_q == 0] <- 0.01 # change zeros to a value so lm works 
 
 # observed temperatures
-temps = read.csv(file.path(data_dir, "temp_dat.csv"))
+#temps = read.csv(file.path(data_dir, "temp_dat.csv"))
 
 # combine discharge & SWE with temp data
-var = swe_q %>% inner_join(temps, by ="wateryear")
+var = swe_q %>% inner_join(all.temp.dat, by ="wateryear")
 
 # sp.test<-apply(var, MARGIN=2, shapiro.test) 
 #TODO : automate this step (e.g. if p < 0.05 log it and remove the og)
@@ -30,4 +37,4 @@ var<-var[,!(names(var) %in% c('cg.swe', 'g.swe','gs.swe','hc.swe', 'lwd.swe','ga
 
 #save variables for use in other scripts
 #TODO: change folder structure so that they are either pre-defined or created -- alt to check w sam about -- remove saving all together because they will stay in the environment?
-write.csv(var, file.path(model_out,'all_vars.csv'), row.names = FALSE)
+#write.csv(var, file.path(model_out,'all_vars.csv'), row.names = FALSE)
