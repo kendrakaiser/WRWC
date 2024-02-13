78516e7179f5b8dc2686c2d3df2cb0104896ac34
Kendra Kaiser <kendra.kaiser@gmail.com>
2024-02-13 09:52:59 -0700

    moved code to run rmd to seperate script

---
+++ code/11_run_rmd.R
@@ -0,0 +1,11 @@
+#TODO: move knit results to shiny so users can click - "download report"
+# knit Model Results PDF
+#detach(package:plyr) #plyr interferes with a grouping function needed for plotting
+#params_list = list(fig_dir_mo_rmd = fig_dir_mo_rmd, set_author = author, 
+#                  todays_date=todays_date, data_dir = data_dir, 
+#                 git_dir = git_dir, input_data = input_data, run_date=run_date)
+
+# update the Rmd to work with figures in the shiny folder and create a "make pdf" button
+# knit PDF - if it doesn't work you can open the 'ModelOutputv2.Rmd' and press 'knit'
+#rmarkdown::render(file.path(git_dir, 'ModelOutputv2.Rmd'), params = params_list, 
+#output_file = file.path(git_dir, paste0("ModelOutput-", end_date, ".pdf")))
