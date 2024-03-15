13ec8e2a196019199e1f9b788528d30fb227969a
Kendra Kaiser <kendra.kaiser@gmail.com>
2024-03-15 17:10:59 -0600

    removed logged swe

--- code/04_data_integration.R
+++ code/04_data_integration.R
@@ -20,15 +20,3 @@ swe_q[swe_q == 0] <- 0.01 # change zeros to a value so lm works
 # combine discharge & SWE with temp data
 var = swe_q %>% merge(all.temp.dat, by ="wateryear", all.x=TRUE)
 
-# sp.test<-apply(var, MARGIN=2, shapiro.test) 
-#TODO : automate this step (e.g. if p < 0.05 log it and remove the og)
-# normailze parameters that have a shapiro.test() < 0.05
-var$cg.log_swe <- log(var$cg.swe)
-var$g.log_swe <- log(var$g.swe)
-var$gs.log_swe <- log(var$gs.swe)
-var$hc.log_swe <- log(var$hc.swe)
-var$lwd.log_swe <- log(var$lwd.swe)
-var$ga.log_swe <- log(var$ga.swe)
-var$bc.log_swe <- log(var$bc.swe)
-var<-var[,!(names(var) %in% c('cg.swe', 'g.swe','gs.swe','hc.swe', 'lwd.swe','ga.swe','bc.swe', 'sr.nj_t', 'sr.aj_t'))]
-
