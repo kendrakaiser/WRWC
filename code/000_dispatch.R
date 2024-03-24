571f650dc982be613be412e174a940ad168c8194
sampcarlson <sam.p.carlson@gmail.com>
2024-03-24 11:56:40 -0600

    adapt to run from shell on sam's computer

--- code/000_dispatch.R
+++ code/000_dispatch.R
@@ -1,1 +1,26 @@
 #ovreall dispatch script, called by cron
+runTask=T   #go/nogo flag
+wd="/home/sam/Documents/R\ Workspace/WRWC_cronTask"
+
+logFile=file(file.path(wd,"log.txt"),open="w")
+sink(file=logFile,append=T,type="output")
+sink(file=logFile,append=T,type="message")
+#run as:
+#Rscript ~/Documents/R\ Workspace/WRWC_cronTask/code/000_dispatch.R
+
+#crontab entry
+#0 5 * * * /usr/sbin/rtcwake -m no -u -t $(date -d '+1 day 05:00' +\%s)
+
+
+
+if(runTask){
+  print("")
+  print("----------------------------------------------------------------------------------------------")
+  print(paste("Run initiated at",Sys.time()))
+  
+  source("~/Documents/R Workspace/WRWC_cronTask/code/00_run_models.R")
+  
+}
+
+sink(type="message")
+sink()#end sink
