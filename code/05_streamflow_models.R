17487e789b074e4555a510f09fc062441ea8691d
Kendra Kaiser <kendra.kaiser@gmail.com>
2024-04-18 13:14:02 -0600

    minor typo

--- code/05_streamflow_models.R
+++ code/05_streamflow_models.R
@@ -182,7 +182,7 @@ cm_model<-function(site, sites, max_var){
   '
   site_vars<- grep(paste(sites, collapse="|"), colnames(var))
   #remove variables from predictor set that don't have data for this year
-  pred.avail- var [var$wateryear == pred.yr,] %>% dplyr::select(wateryear, all_of(site_vars), all_of(swe_cols), 
+  pred.avail<- var [var$wateryear == pred.yr,] %>% dplyr::select(wateryear, all_of(site_vars), all_of(swe_cols), 
         all_of(wint_t_cols), -all_of(c(tot_vol_cols, runoff_cols, irr_vol_cols)))# %>% filter(complete.cases(.))
   na.vars<- names(pred.avail)[is.na(pred.avail[1,])] 
   
