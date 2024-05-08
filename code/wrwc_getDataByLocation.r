2f654ecbc9a7dfa48cf889f90a8c9d5d4c63917c
Kendra Kaiser <kendra.kaiser@gmail.com>
2024-05-08 09:42:08 -0600

    modified for gw data pull

--- code/wrwc_getDataByLocation.r
+++ code/wrwc_getDataByLocation.r
@@ -1,4 +1,5 @@
 library(sf)
+library(DBI)
 
 #location attributes, note the 'metrics' column.  You can also see all the locations in the app map.
 locationAttributes=st_read(conn,query = "SELECT locationid, name, metrics, locationgeometry FROM locationattributes;")
@@ -11,7 +12,7 @@ xy=st_coordinates(locationAttributes[locationAttributes$locationid==targetLID,])
 nearLocations=dbGetQuery(conn,sqlInterpolate(conn,"SELECT * FROM locations WHERE ST_DWITHIN(locations.geometry, ST_POINT(?x,?y, 26911),?dist);",
                                              x=xy[1],y=xy[2],dist=distance))
 
-dbGetQuery(conn,paste0("SELECT metric, value, datetime, data.locationid, locations.name FROM
+out<-dbGetQuery(conn,paste0("SELECT metric, value, datetime, data.locationid, locations.name FROM
                        data LEFT JOIN locations ON data.locationid = locations.locationid
                        WHERE data.locationid IN ('",
                        paste(nearLocations$locationid,collapse="', '"),
